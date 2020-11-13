7e8470c45739a135200ae3a442722b7955a6d0b8
Jessica Mattick <jmatty1661@gmail.com>
2020-11-12 19:39:00 -0600

    add code

--- asteroid_ht.py
+++ asteroid_ht.py
@@ -1,19 +1,11 @@
-# asteroids_hyperparameter_tuning
-
-Data was downloaded from: https://www.kaggle.com/shrutimehta/nasa-asteroids-classification
-
-Data can be found in nasa.csv
-
-The dataset contains information about 4687 asteroids and a hazardous classification of True or False.
-
-The dataset was read using pandas. Features most likely to impact Hazard level of an asteroid were selected. 
-The samples in the dataset were shuffled using numpy to 
-allow the data to be split for training and testing. 20% of the dataset was reserved for the final 
-test of the model. 
-
-```python
 import pandas as pd
 import numpy as np
+import math
+import itertools
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 
 # read data using pandas
 data = pd.read_csv("nasa.csv")
@@ -30,7 +22,7 @@ features = ['Absolute Magnitude', 'Est Dia in KM(min)',
             'Perihelion Arg', 'Aphelion Dist', 'Perihelion Time', 'Mean Anomaly',
             'Mean Motion']
 
-# shuffle indices
+# shuffle indces
 np.random.permutation(data)
 
 # get length of data and determine index of 80%
@@ -47,14 +39,6 @@ y_train = train.Hazardous
 # set X and y for test dataset
 X_test = test[features]
 y_test = test.Hazardous
-```
-
-The data was scaled using sklearn's StandardScaler(). The scaler was fit on all of the 
-training data.
-
-```python
-from sklearn.preprocessing import StandardScaler
-
 # scale data using sklearn StandardScaler
 sc = StandardScaler()
 
@@ -64,15 +48,7 @@ sc.fit(X_train)
 # transform training and validation data
 X_train = sc.transform(X_train)
 X_test = sc.transform(X_test)
-```
 
-A function `test_model` was created to score the models on a given dataset. 
-The function returns the F1 score of the model. The F1 score was used instead of accuracy since the distribution of the 
-hazard class is not uniform with the majority of the samples not being hazardous.
-The F1 score was calculated by using sklearn's f1_score method.
-
-```python
-from sklearn.metrics import f1_score
 
 def test_model(train_X, val_X, train_y, val_y, model):
     """Function to return the F1 score of a given model and dataset"""
@@ -84,13 +60,8 @@ def test_model(train_X, val_X, train_y, val_y, model):
 
     # return accuracy
     return f1_score(val_y, val_predict)
-``` 
 
-An n-fold cross validation method was implemented. This method splits a dataset into 
-n chunks and evaluates the model iteratively over n-1 parts. 
-The function returns a list of scores.
 
-```python
 def n_fold_cross_validation(dataset, features, target, model, sc, n=5):
     # get length of data and determine index of 80%
     size = len(dataset)
@@ -127,17 +98,7 @@ def n_fold_cross_validation(dataset, features, target, model, sc, n=5):
         start = end
 
     return scores
-```
 
-Next, grid search was implemented. The `grid_search` method 
-evaluates a model on a given dataset for every parameter combination 
-given to the method. For each parameter combination, the n_fold_cross_validation 
-is called to get average score of the model. The function returns 
-3 lists. The first contains the parameters used in the model, the second contains the 
-average score for each model, and the third contains the standard deviation of the scores 
-of each model. 
-
-```python 
 
 def grid_search(model, params, dataset, features, target, sc, n=5):
     # Format parameters to use in model
@@ -165,16 +126,12 @@ def grid_search(model, params, dataset, features, target, sc, n=5):
         std.append(np.std(curr_scores))
 
     return param_info, scores, std
-```
 
-The SVC model from sklearn was used on this dataset. The hyper-parameters 
-tuned using grid_search were the C and kernel. The parameter options were 
-given in the form of a dictionary containing a list of potential parameter values.
 
-```python
+# SVM
+
 params = {'C': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['linear', 'rbf']}
 
-# use grid search
 grid_p, grid_s, grid_e = grid_search(SVC, params, train, features, "Hazardous", sc, 5)
 
 grid_p_str = [str(p) for p in grid_p]
@@ -189,22 +146,6 @@ plt.tight_layout()
 plt.savefig("grid_search.png")
 plt.close()
 
-```
-
-The results of the grid_search using the SVC classifier to tune
-the C and kernel are shown in the plot below. There was a variety of success between combinations of parameters.
-
-![grid_plot](grid_search.png)
-
-The model with the highest average F1 score was selected to be tested using the test dataset. 
-
-best model: {'C': 1, 'kernel': 'linear'}
-
-F1 score average: 0.8565524132689802
-
-F1 score std: 0.0170101986512874
-
-```python
 # Find index of max score
 max_idx = np.argmax(grid_s)
 max_score = grid_s[max_idx]
@@ -228,9 +169,7 @@ plt.title("F1 score of Training and Test Datasets")
 plt.tight_layout()
 plt.savefig("train_test_compare.png")
 plt.close()
-```
 
-The F1 score of the test dataset was 0.8634. This value was within the standard deviation of the n-fold cross validated training scores
-for the same model. The small differences are due to the different samples present in the test and training datasets. 
+print('Test dataset on best model: ' + str(score_nocv))
 
-![test_res](train_test_compare.png)
+
